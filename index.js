acd140d629b2e381559a84a068e4d9a791f896fe
Pandabotic <ianelijah0730@yahoo.com>
2024-07-12 09:41:10 -0700

    Publish

---
+++ index.js
@@ -0,0 +1,70 @@
+var swiper = new Swiper(".home", {
+    spaceBetween: 0,
+    loop: true,
+    centeredSlides: true,
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
+  var swiper = new Swiper(".coming-soon", {
+    spaceBetween: 10,
+    loop: true,
+    centeredSlides: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+    breakpoints: { //Depending on the width size, display that number of movies
+        0: {
+            slidesPerView: 2,
+        },
+        568: {
+            slidesPerView: 3,
+        },
+        768: {
+            slidesPerView: 5,
+        },
+        968: {
+            slidesPerView: 6,
+        },
+    }
+  });
+
+  var swiper = new Swiper(".movies-out-now", {
+    spaceBetween: 10,
+    loop: true,
+    centeredSlides: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+    breakpoints: { //Depending on the width size, display that number of movies
+        0: {
+            slidesPerView: 2,
+        },
+        568: {
+            slidesPerView: 3,
+        },
+        768: {
+            slidesPerView: 5,
+        },
+        968: {
+            slidesPerView: 6,
+        },
+    }
+  });
+
+const menuIcon = document.querySelector('#menu-icon')
+const navbar = document.querySelector('.navbar')
+
+menuIcon.onclick = function() {
+    menuIcon.classList.toggle("fa-x");
+    navbar.classList.toggle('active');
+}
+
